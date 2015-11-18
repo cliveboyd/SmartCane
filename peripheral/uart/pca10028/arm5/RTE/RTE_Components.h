52ea937280ddcb473fe0cb253e2d186828699be0
wanyancan.400d-90506 <wanyancanat gmail dot com>
2015-11-18 16:05:26 +1100

    Update re git

---
+++ peripheral/uart/pca10028/arm5/RTE/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'uart_pca10028' 
+ * Target:  '51822' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define BSP_DEFINES_ONLY
+
+#endif /* RTE_COMPONENTS_H */
